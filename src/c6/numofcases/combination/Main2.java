2e3ea82f60eca47490b646bf74227c21ad8086f9
DS0708 <jonum12312@gmail.com>
2025-02-20 19:30:44 +0900

    Combination, Permutation 연습

---
+++ src/c6/numofcases/combination/Main2.java
@@ -0,0 +1,28 @@
+package c6.numofcases.combination;
+
+import java.util.Stack;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int n=5;
+        int r=3;
+        Stack<Integer> stack = new Stack<>();
+        System.out.println(combi(stack,n,r));
+    }
+    public static int combi(Stack<Integer> stack, int n, int r){
+        if(r==0){
+            for(int s : stack) System.out.print(s+" ");
+            System.out.println();
+            return 1;
+        }else{
+            int ret = 0;
+            int smallest = stack.isEmpty() ? 1 : stack.peek() + 1;
+            for(int next=smallest; next<=n; next++){
+                stack.push(next);
+                ret += combi(stack, n , r-1);
+                stack.pop();
+            }
+            return ret;
+        }
+    }
+}
---
+++ src/c6/numofcases/permutation/Main2.java
@@ -0,0 +1,29 @@
+package c6.numofcases.permutation;
+
+import java.util.Stack;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int n = 5;
+        int r = 3;
+        Stack<Integer> stack = new Stack<>();
+        System.out.println(permu(stack,5,3));
+    }
+    public static int permu(Stack<Integer> stack,int n, int r) {
+        if(r==0){
+            for(int s : stack) System.out.print(s+" ");
+            System.out.println();
+            return 1;
+        }else{
+            int ret = 0;
+            for(int i=1; i<=n; i++){
+                if(!stack.contains(i)){
+                    stack.push(i);
+                    ret += permu(stack,n,r-1);
+                    stack.pop();
+                }
+            }
+            return ret;
+        }
+    }
+}
