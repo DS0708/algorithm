bc68ed097751c52b5dc9a1d276f8f5721bd50e95
DS0708 <jonum12312@gmail.com>
2025-03-05 15:20:11 +0900

    LIS : DP로 풀기 (R)

--- src/c8/dp/lis/DPNoMaxLen.java
+++ src/c8/dp/lis/DPNoMaxLen.java
@@ -6,8 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-//시간복잡도 O(N^2)
-public class DP {
+public class DPNoMaxLen {
     static int N;
     static int[] Arr;
     static int[] Cache;
@@ -19,36 +18,31 @@ public class DP {
             //init
             N = Integer.parseInt(br.readLine());
             Arr = new int[N];
-            Cache = new int[N];
+            //Cache[0]은 전체 부분 수열에서 가장 긴 길이, 기존의 Cache값을 하나씩 뒤로 미뤘다고 생각하면 됨
+            Cache = new int[N+1];
             Arrays.fill(Cache, -1);
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int i=0; i<N; i++){
                 Arr[i] = Integer.parseInt(st.nextToken());
             }
-            //[5, 2, 3, 4] 의 경우 lisDP(0)만 호출하면 답은 1이다 -> 그래서 전체를 시작점으로 돌려야 함
-            int maxLen = 0;
-            for(int i=0; i<N; i++){
-                maxLen = Math.max(maxLen, lisDP(i));
-            }
-            //output
-            System.out.println(maxLen);
+            //output, -1을 넣어 모든 수가 시작점이 되도록 만듦
+            //또한 임의로 1개를 추가했기 때문에 1개를 빼줘야 함
+            System.out.println(lisDP(-1)-1);
         }
     }
     //lisDP(start) = Arr[start]에서 시작하는 증가 수열의 최대 길이
     public static int lisDP(int start){
-        //base case는 필요 없음
-
         //Memoization
-        if(Cache[start] != -1) return Cache[start];
+        if(Cache[start+1] != -1) return Cache[start+1];
         //Logic
         //자기 자신을 무조건 하나 포함할 수 있으므로
-        Cache[start] = 1;
+        Cache[start+1] = 1;
         for(int next=start+1; next<N; next++){
-            if(Arr[start] < Arr[next]){
-                Cache[start] = Math.max(Cache[start], 1 + lisDP(next));
+            if(start==-1 || Arr[start] < Arr[next]){
+                Cache[start+1] = Math.max(Cache[start+1], 1 + lisDP(next));
             }
         }
-        return Cache[start];
+        return Cache[start+1];
     }
 }
 
