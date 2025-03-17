95ed4634322226bda38acffcc3a8bcad344e1789
DS0708 <jonum12312@gmail.com>
2025-03-17 22:34:25 +0900

    forEach 문제점.

---
+++ src/graph/c28/wordchain/ListExample.java
@@ -0,0 +1,18 @@
+package graph.c28.wordchain;
+
+import java.util.*;
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<Integer>[][] graph = new ArrayList[5][5];
+
+        //이렇게 선언하면 하나의 참조값이 동일한 row에 할당된다 !!
+        Arrays.stream(graph).forEach(r->Arrays.fill(r, new ArrayList<>()));
+
+        graph[0][0].add(1);
+        graph[0][2].add(2);
+        graph[0][3].add(2);
+
+        for(int i : graph[0][0]) System.out.println(i);
+    }
+}
