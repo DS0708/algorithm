1a6fcfce6754a08eef4e402a2dab3319d8d0e517
DS0708 <jonum12312@gmail.com>
2025-03-19 10:45:23 +0900

    List에서 특정 인덱스 범위 뒤집기 연산

---
+++ src/graph/c29/sortgame/CollectionReverse.java
@@ -0,0 +1,14 @@
+package graph.c29.sortgame;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionReverse {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=1; i<=10; i++) list.add(i);
+        Collections.reverse(list.subList(0,3));
+        for(int i : list) System.out.print(i + " ");
+    }
+}
